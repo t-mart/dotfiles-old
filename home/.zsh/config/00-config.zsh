39a6b7152f31791c375210d5c5c60650a90da9ab
Tim Martin <tim@timmart.in>
2016-12-06 17:10:43 -0600

    describe less colors

--- home/.zsh/config/00-config.zsh
+++ home/.zsh/config/00-config.zsh
@@ -79,15 +79,15 @@ fi
 # ====
 # less
 # ====
-export PAGER=${PAGER:-less}
-# support colors in less
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;31m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;44;33m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[01;32m'
+export PAGER=less
+# support colors and styles in less
+export LESS_TERMCAP_mb=$'\E[01;31m'     # start blink
+export LESS_TERMCAP_md=$'\E[01;31m'     # start bold
+export LESS_TERMCAP_so=$'\E[01;44;33m'  # start standout
+export LESS_TERMCAP_ue=$'\E[0m'         # start underline
+export LESS_TERMCAP_me=$'\E[0m'         # stop bold, blink, underline
+export LESS_TERMCAP_se=$'\E[0m'         # stop standout
+export LESS_TERMCAP_us=$'\E[01;32m'     # stop underline
 
 # ==============
 # job management
